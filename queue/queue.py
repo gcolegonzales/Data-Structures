6ee9eb73690bbedb218a14be37619b2c795b28ca
Cole Gonzales <colegonzales@Coles-MacBook-Pro.local>
2020-06-17 20:56:30 -0500

    Day one MVP

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,63 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __str__(self):
+#         return f"Your queue is good!"
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self) == 0:
+#             return None
+#         else:
+#             dequeued_item = self.storage[0]
+#             self.storage.pop(0)
+#             return dequeued_item
+
+class Node: 
+    def __init__(self, value = None, next_node = None ):
+        self.value = value
+        self.next_node = next_node 
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.first_node = None
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        my_node = Node(value)
+
+        if self.first_node == None:
+            self.first_node = my_node
+            return 
+
+        current_node = self.first_node
+
+        while current_node.next_node != None:
+            current_node = current_node.next_node
+
+        current_node.next_node = my_node
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+
+        value = self.first_node.value
+
+        self.first_node = self.first_node.next_node
+        self.size -= 1
+
+        return value
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,74 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             # .pop in Python, accepts an index as a parameter,
+#             # if no param is given, it defauls to -1 (the last index)
+#             return self.storage.pop()
+
+#         else:
+#             return None
+
+class Node: 
+    def __init__(self, value = None, next_node = None ):
+        self.value = value
+        self.next_node = next_node 
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.first_node = None
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        new_node = Node(value)
+
+        if self.first_node == None:
+            self.first_node = new_node
+            return self.first_node
+
+        current_node = self.first_node
+
+        while current_node.next_node != None:
+            current_node = current_node.next_node
+
+        current_node.next_node = new_node
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+
+        current_node = self.first_node
+
+        if self.first_node.next_node == None:
+            self.size = 0
+            self.first_node = None
+            return current_node.value
+
+        prior_node = self.first_node
+        current_node = self.first_node.next_node
+
+        while current_node.next_node != None:
+            new_prior_node = current_node
+            current_node = current_node.next_node
+            prior_node = new_prior_node
+
+        prior_node.next_node = None
+        self.size -= 1
+
+        return current_node.value
